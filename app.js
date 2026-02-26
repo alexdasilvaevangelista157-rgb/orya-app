12b98a53f31fee808c64dfd3c5b3c61377fca027
alexdasilvaevangelista157-rgb <alexdasilvaevangelista157@gmail.com>
2026-02-26 01:55:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-function login() {
-
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  if(email === "" || password === ""){
-    alert("Fill in all fields");
-    return;
-  }
-
+function login(){
   document.getElementById("login-screen").style.display = "none";
   document.getElementById("app-screen").style.display = "block";
-
 }
 
 function register(){
-  alert("Registration system coming soon.");
+  alert("Conta criada (modo teste)");
+  document.getElementById("login-screen").style.display = "none";
+  document.getElementById("app-screen").style.display = "block";
 }
